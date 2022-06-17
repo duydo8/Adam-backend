4fc2b40e3befdf89d5bc3b9a93f8a8d74d566748
duydo8 <duydogch123456@gmail.com>
2022-06-17 13:23:50 +0700

    fix debug heroku

--- src/main/java/com/example/adambackend/controller/admin/DistrictController.java
+++ src/main/java/com/example/adambackend/controller/admin/DistrictController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("admin/district")
 public class DistrictController {
     @Autowired
