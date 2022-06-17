bc16444c1f04ef8b86259098da752c0cc10d7007
duydo8 <duydogch123456@gmail.com>
2022-06-17 13:08:02 +0700

    cross orgin

--- src/main/java/com/example/adambackend/controller/admin/ColorController.java
+++ src/main/java/com/example/adambackend/controller/admin/ColorController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("admin/color")
 public class ColorController {
     @Autowired
--- src/main/java/com/example/adambackend/controller/admin/DistrictController.java
+++ src/main/java/com/example/adambackend/controller/admin/DistrictController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("admin/district")
 public class DistrictController {
     @Autowired
