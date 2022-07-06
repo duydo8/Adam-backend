5f56c7f7e658ec1ea1eb761137a0fc2141160c66
duydo8 <duydogch123456@gmail.com>
2022-07-06 14:28:52 +0700

    fix cart and order

--- src/main/java/com/example/adambackend/exception/HandleExceptionDemo.java
+++ src/main/java/com/example/adambackend/exception/HandleExceptionDemo.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HandleExceptionDemo {
-    private int code;
+    private int status;
     private String message;
 }
