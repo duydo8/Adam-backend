8dfed8d8c8a6e8f3a0bb8a8da38b32e1444ed15a
duydo8 <duydogch123456@gmail.com>
2022-07-07 09:45:36 +0700

    update delete

--- src/main/java/com/example/adambackend/config/TwilioSendSms.java
+++ src/main/java/com/example/adambackend/config/TwilioSendSms.java
@@ -5,11 +5,13 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class TwilioSendSms {
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     public void sendCode(String phoneNumber,String code) {
 
         Twilio.init(
-                System.getenv("AC07311f9dc9c439a7e2e5ca5fb8e2ef2a"),
-                System.getenv("eee2d0a92e9c0de8efd1dcc8dddfe101"));
+                System.getenv("ACCOUNT_SID"),
+                System.getenv("AUTH_TOKEN"));
 
         Message.creator(new PhoneNumber("+19036229068"),new PhoneNumber(phoneNumber),
                 "Mã xác nhận của bạn là " + code +" sẽ hết hạn sau 30p").create();
--- src/main/java/com/example/adambackend/repository/CategoryRepository.java
+++ src/main/java/com/example/adambackend/repository/CategoryRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     @Query(value = "select DISTINCT c2.id,c2.category_name,c2.is_deleted,c2.create_date,c2.category_parent_id,c2.is_active               from categories c1, categories c2 \n" +
-            "where c2.id= c1.category_parent_id or c2.category_parent_id is null and c2.is_deleted=0 and is_active=1", nativeQuery = true)
+            "where c2.id= c1.category_parent_id or c2.category_parent_id is null and c2.is_deleted=0 and c2.is_active=1", nativeQuery = true)
     List<Category> findAllCategoryParentId();
 
     List<Category> findByCategoryParentId(int parentId);
