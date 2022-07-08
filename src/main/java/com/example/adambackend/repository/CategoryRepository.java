c358ec1c6036520b905aff018c60ad7fdf16dc94
duydo8 <duydogch123456@gmail.com>
2022-07-08 23:27:39 +0700

    update product

--- src/main/java/com/example/adambackend/repository/CategoryRepository.java
+++ src/main/java/com/example/adambackend/repository/CategoryRepository.java
@@ -14,8 +14,8 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     @Query(value = "select DISTINCT c2.id,c2.category_name,c2.is_deleted,c2.create_date,c2.category_parent_id,c2.is_active from categories c1, categories c2 \n" +
-            "where c2.id= c1.category_parent_id or c2.category_parent_id is null and c2.is_deleted=0 and c2.is_active=1 and c1.is_deleted=0 and c1.is_active=1", nativeQuery = true)
-    List<Category> findAllCategoryParentId();
+            "where c2.id= c1.category_parent_id or c2.category_parent_id is null or c1.category_parent_id is null and c2.is_deleted=0 and c2.is_active=1 and c1.is_deleted=0 and c1.is_active=1", nativeQuery = true)
+    List<Category>  findAllCategoryParentId();
 @Query(value = "select * from categories where category_parent_id=?1 and is_active=1 and is_deleted=0",nativeQuery = true)
     List<Category> findByCategoryParentId(int parentId);
     @Modifying
