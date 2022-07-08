aff9085b867b11b4cf5ff73d390d06f920f0b1f9
duydo8 <duydogch123456@gmail.com>
2022-07-08 10:50:52 +0700

    update delete

--- src/main/java/com/example/adambackend/repository/AddressRepository.java
+++ src/main/java/com/example/adambackend/repository/AddressRepository.java
@@ -12,12 +12,12 @@ import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
-    @Query(value = "select * from address where account_id=?1 where is_active=1 and is_deleted=0 ",nativeQuery = true)
+    @Query(value = "select * from address where account_id=?1 and is_active=1 and is_deleted=0 ",nativeQuery = true)
     List<Address> findByAccountId(Integer accountId);
 //    @Modifying
 //    @Transactional
 //    @Query(value = "update address set is_deleted=1 and is_active=0 where id=?1",nativeQuery = true)
 //    void updateAddressDeleted(Integer id);
-    @Query(value = "select * from address where is_active=1 and is_delete=0",nativeQuery = true)
+    @Query(value = "select * from address where is_active=1 and is_deleted=0",nativeQuery = true)
     List<Address> findAlls();
 }
