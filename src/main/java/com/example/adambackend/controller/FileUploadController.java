2a87d53b760e2653096db91e264b27c6b95c8864
duydo8 <duydogch123456@gmail.com>
2022-07-12 10:26:31 +0700

    update findOption

---
+++ src/main/java/com/example/adambackend/controller/FileUploadController.java
@@ -0,0 +1,45 @@
+package com.example.adambackend.controller;
+
+import com.example.adambackend.payload.response.Response;
+import com.example.adambackend.service.impl.FileStorageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin(value = "*", maxAge = 3600)
+public class FileUploadController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/uploadFile")
+    public Response uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new Response(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<Response> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+}
