dd275ece03c9fdafe02c7b398913d83d6883ae24
duydv <duydv>
2025-02-24 17:27:33 +0700

    update twillio

--- src/main/java/com/example/adambackend/config/TwilioSendSms.java
+++ src/main/java/com/example/adambackend/config/TwilioSendSms.java
@@ -3,19 +3,27 @@ package com.example.adambackend.config;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 
 public class TwilioSendSms {
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    public void sendCode(String phoneNumber, Integer code) {
+  @Value("${twilio.account.sid}")
+  private String accountSid;
 
-        Twilio.init(
-                "AC2997aff34046863723298d818dd01090",
-                "640ebd17a042422815e499ae5d5fa8dc");
+  @Value("${twilio.account.token}")
+  private String authToken;
 
-        Message.creator(new PhoneNumber(phoneNumber), new PhoneNumber("+14173843028"),
-                "Your verification code is: " + code).create();
+  @Value("${twilio.account.phone-number}")
+  private String twilioPhoneNumber;
 
-    }
+  @Value("${twilio.message.verify}")
+  private String messageCode;
+
+  public void sendCode(String phoneNumber, Integer code) {
+
+    Twilio.init(accountSid, authToken);
+
+    Message.creator(new PhoneNumber(phoneNumber), new PhoneNumber(twilioPhoneNumber),
+        messageCode + " " + code).create();
+  }
 }
